6f73123bf50cde6754afd361b41db08c763b541e
Burdsall <tyler.burdsall@sulzer.com>
2018-11-21 17:14:59 -0800

    Added conditional compilation flags based on if boost is being used

--- src/combigen.cpp
+++ src/combigen.cpp
@@ -165,66 +165,15 @@ static const void display_help(void)
          << "   -v             Display version number" << "\n";
 }
 
-static const void generate_all(const unsigned long long &max_size, const generation_args &args)
-{
-    if (!args.display_json)
-    {
-        if (args.display_keys)
-        {
-            display_csv_keys(args.pc.keys, args.delim);
-        }
-    }
-    else
-    {
-        cout << "[\n";
-    }
-    const unsigned long long last = max_size - 1;
-    for (unsigned long long i = 0; i < max_size; ++i)
-    {
-        vector<string> result = lazy_cartesian_product::entry_at(args.pc.combinations, i);
-        output_result(result, args, true);
-        if (args.display_json && i != last)
-        {
-            cout << ",";
-        }
-    }
-    if (args.display_json)
-    {
-        cout << "]\n";
-    }
-}
-
-static const void generate_random_samples(const vector<unsigned long long> &range, const generation_args &args)
-{
-    if (!args.display_json)
-    {
-        if (args.display_keys)
-        {
-            display_csv_keys(args.pc.keys, args.delim);
-        }
-    }
-    else
-    {
-        cout << "[\n";
-    }
-    for (const unsigned long long &i: range)
-    {
-        vector<string> result = lazy_cartesian_product::entry_at(args.pc.combinations, i);
-        output_result(result, args, true);
-        if (args.display_json && &i != &range.back())
-        {
-            cout << ",";
-        }
-    }
-    if (args.display_json)
-    {
-        cout << "]\n";
-    }
-}
 
 static const void generate_random_samples_performance_mode(const generation_args &args)
 {
+#ifdef USE_BOOST
+    const uint1024_t sample_size(args.sample_size);
+    const vector<vector<string>> results = lazy_cartesian_product::boost_generate_samples(args.pc.combinations, sample_size);
+#else
     const vector<vector<string>> results = lazy_cartesian_product::generate_samples(args.pc.combinations, args.sample_size);
+#endif
     
     if (!args.display_json)
     {
@@ -294,7 +243,11 @@ static const void output_result(const vector<string> &result, const generation_a
 
 static const void parse_args(const generation_args &args)
 {
-    unsigned long long max_size = lazy_cartesian_product::compute_max_size(args.pc.combinations);
+#ifdef USE_BOOST
+    const uint1024_t max_size = lazy_cartesian_product::boost_compute_max_size(args.pc.combinations);
+#else
+    const unsigned long long max_size = lazy_cartesian_product::compute_max_size(args.pc.combinations);
+#endif
     if (args.generate_all_combinations)
     {
         generate_all(max_size, args);
@@ -302,15 +255,32 @@ static const void parse_args(const generation_args &args)
     }
     else
     {
-        if (args.sample_size == 0 && args.entry_at_provided && !args.generate_all_combinations)
+#ifdef USE_BOOST
+        const uint1024_t sample_size(args.sample_size);
+#else
+        const unsigned long long sample_size = stoull(args.sample_size, 0, 10);
+#endif
+        if (sample_size == 0 && args.entry_at_provided && !args.generate_all_combinations)
         {
-            vector<string> result = lazy_cartesian_product::entry_at(args.pc.combinations, args.entry_at);
+#ifdef USE_BOOST
+            const uint1024_t entry_at(args.entry_at);
+            vector<string> result = lazy_cartesian_product::boost_entry_at(args.pc.combinations, args.entry_at);
             output_result(result, args, false);
             exit(0);
+#else
+            const unsigned long long entry_at = stoull(args.entry_at, 0, 10);
+            vector<string> result = lazy_cartesian_product::entry_at(args.pc.combinations, entry_at);
+            output_result(result, args, false);
+            exit(0);
+#endif
         }
-        else if (args.sample_size >= 0)
+        else if (sample_size >= 0)
         {
-            const unsigned long long n = args.sample_size;
+#ifdef USE_BOOST
+            const uint1024_t n(args.sample_size);
+#else
+            const unsigned long long n = stoull(args.sample_size, 0, 10);
+#endif
             if (n > max_size)
             {
                 cerr << "ERROR: Sample size cannot be greater than maximum possible combinations\n";
@@ -322,7 +292,11 @@ static const void parse_args(const generation_args &args)
             }
             else
             {
+#ifdef USE_BOOST
+                vector<uint1024_t> range = lazy_cartesian_product::boost_generate_random_indices(n, max_size);
+#else
                 vector<unsigned long long> range = lazy_cartesian_product::generate_random_indices(n, max_size);
+#endif
                 generate_random_samples(range, args);
             }
             exit(0);
@@ -390,3 +364,118 @@ static const possible_combinations parse_stdin(const string &input)
     return pc;
 }
 
+#ifdef USE_BOOST
+static const void generate_random_samples(const vector<uint1024_t> &range, const generation_args &args)
+{
+    if (!args.display_json)
+    {
+        if (args.display_keys)
+        {
+            display_csv_keys(args.pc.keys, args.delim);
+        }
+    }
+    else
+    {
+        cout << "[\n";
+    }
+    for (const uint1024_t &i: range)
+    {
+        vector<string> result = lazy_cartesian_product::boost_entry_at(args.pc.combinations, i.convert_to<string>());
+        output_result(result, args, true);
+        if (args.display_json && &i != &range.back())
+        {
+            cout << ",";
+        }
+    }
+    if (args.display_json)
+    {
+        cout << "]\n";
+    }
+}
+
+static const void generate_all(const uint1024_t &max_size, const generation_args &args)
+{
+    if (!args.display_json)
+    {
+        if (args.display_keys)
+        {
+            display_csv_keys(args.pc.keys, args.delim);
+        }
+    }
+    else
+    {
+        cout << "[\n";
+    }
+    const uint1024_t last = max_size - 1;
+    for (uint1024_t i = 0; i < max_size; ++i)
+    {
+        vector<string> result = lazy_cartesian_product::boost_entry_at(args.pc.combinations, i.convert_to<string>());
+        output_result(result, args, true);
+        if (args.display_json && i != last)
+        {
+            cout << ",";
+        }
+    }
+    if (args.display_json)
+    {
+        cout << "]\n";
+    }
+}
+#else
+static const void generate_all(const unsigned long long &max_size, const generation_args &args)
+{
+    if (!args.display_json)
+    {
+        if (args.display_keys)
+        {
+            display_csv_keys(args.pc.keys, args.delim);
+        }
+    }
+    else
+    {
+        cout << "[\n";
+    }
+    const unsigned long long last = max_size - 1;
+    for (unsigned long long i = 0; i < max_size; ++i)
+    {
+        vector<string> result = lazy_cartesian_product::entry_at(args.pc.combinations, i);
+        output_result(result, args, true);
+        if (args.display_json && i != last)
+        {
+            cout << ",";
+        }
+    }
+    if (args.display_json)
+    {
+        cout << "]\n";
+    }
+}
+
+static const void generate_random_samples(const vector<unsigned long long> &range, const generation_args &args)
+{
+    if (!args.display_json)
+    {
+        if (args.display_keys)
+        {
+            display_csv_keys(args.pc.keys, args.delim);
+        }
+    }
+    else
+    {
+        cout << "[\n";
+    }
+    for (const unsigned long long &i: range)
+    {
+        vector<string> result = lazy_cartesian_product::entry_at(args.pc.combinations, i);
+        output_result(result, args, true);
+        if (args.display_json && &i != &range.back())
+        {
+            cout << ",";
+        }
+    }
+    if (args.display_json)
+    {
+        cout << "]\n";
+    }
+}
+#endif
--- src/combigen.h
+++ src/combigen.h
@@ -12,13 +12,14 @@
 #include <iostream>
 #include <iterator>
 #include <iomanip>
-#include <vector>
 #include <string>
 #include <cstdlib>
 #include <stdexcept>
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 #include "lib/win-getopt/getopt.h"
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace boost::multiprecision;
 #else
 #include <unistd.h>
 #endif
@@ -28,11 +29,20 @@
 #include "lib/nlohmann/json/single_include/nlohmann/json.hpp"
 #include "lib/iamtheburd/lazy-cartesian-product/lazy-cartesian-product.hpp"
 
+#ifdef USE_BOOST
+#include <boost/container/vector.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+using boost::container::vector;
+using namespace boost::multiprecision;
+#else
+#include <vector>
+using std::vector;
+using std::stoull;
+#endif
 
 using std::cout;
 using std::cin;
 using std::cerr;
-using std::vector;
 using std::string;
 using std::ifstream;
 using std::istringstream;
@@ -53,8 +63,8 @@ struct generation_args
     possible_combinations           pc;
     string                          input;
     string                          delim = ",";
-    unsigned long long              entry_at = 0;
-    unsigned long long              sample_size = 0;
+    string                          entry_at = "0";
+    string                          sample_size = "0";
     bool                            generate_all_combinations = false;
     bool                            display_keys = false;
     bool                            display_json = false;
@@ -64,12 +74,19 @@ struct generation_args
 
 static const void                   display_csv_keys(const vector<string> &keys, const string &delim);
 static const void                   display_help(void);
-static const void                   generate_all(const unsigned long long &max_size, const generation_args &args);
-static const void                   generate_random_samples(const vector<unsigned long long> &range, const generation_args &args);
+
 static const void                   generate_random_samples_performance_mode(const generation_args &args);
 static const void                   output_result(const vector<string> &result, const generation_args &args, const bool &for_optimization);
 static const void                   parse_args(const generation_args &args);
 static const possible_combinations  parse_file(const string &input);
 static const possible_combinations  parse_stdin(const string &input);
+
+#ifdef USE_BOOST
+static const void                   generate_all(const uint1024_t &max_size, const generation_args &args);
+static const void                   generate_random_samples(const vector<uint1024_t> &range, const generation_args &args);
+#else
+static const void                   generate_all(const unsigned long long &max_size, const generation_args &args);
+static const void                   generate_random_samples(const vector<unsigned long long> &range, const generation_args &args);
+#endif
 
 #endif
