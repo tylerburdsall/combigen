b7c7bf7b580d2ceb65705396ca8128b63b4b4ceb
Tyler Burdsall <tylerburdsall@gmail.com>
2018-11-28 17:26:05 -0800

    Add GNU GPLv3 License to each source file

--- src/cli_functions.cpp
+++ src/cli_functions.cpp
@@ -1,3 +1,21 @@
+/* cli_functions.cpp
+ *
+ * Copyright (C) 2018 Tyler Burdsall
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef CLI_FUNCTIONS_CPP
 #define CLI_FUNCTIONS_CPP
 
@@ -12,7 +30,8 @@ const void display_help(void)
          << "   -n <index>     Generate combination at nth index" << "\n\n"
          << "   -i <input>     Take the given .json file as input. Otherwise, input will come" << "\n"
          << "                  from stdin." << "\n"
-         << "                  Example: \"{ \"foo\": [ \"a\", \"b\", \"c\" ], \"bar\": [ \"1\", \"2\" ] }\"" << "\n\n"
+         << "                  Example: \"{ \"foo\": [ \"a\", \"b\", \"c\" ], \"bar\": [ \"1\", \"2\" ] }\"" << "\n"
+         << "                  Or:      \"[ [\"1\", \"2\"], [\"3\", \"4\", \"a\", \"b\"] ]\"" << "\n\n"
          << "   -t <type>      Output type (csv or json). Defaults to csv" << "\n\n"
          << "   -r <size>      Generate a random sample of size r from" << "\n"
 	 << "                  the possible set of combinations" << "\n\n"
